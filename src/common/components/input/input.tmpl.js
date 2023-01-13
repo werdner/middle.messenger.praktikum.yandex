f94b9e48dceb167222592d8d4f00663801dc3321
werdner <werdner@yandex-team.ru>
2023-01-13 18:41:40 +0300

    sprint_1: Добавил дефолтные значения

--- src/common/components/input/input.tmpl.js
+++ src/common/components/input/input.tmpl.js
@@ -1,6 +1,11 @@
 import * as styles from "../../pages/profilePage/styles.module.css";
 
-export const input = (type, name, placeholder, additionalClassName) => {
+export const input = (
+    type = 'text',
+    name = '',
+    placeholder = '',
+    additionalClassName = ''
+) => {
     return `
         <input
             className="${styles['item__field']} ${styles[additionalClassName]}"
